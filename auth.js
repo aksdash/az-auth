9b70c00c7f2e41816abbd90d662490dec1ffdf8e
Akash Deep <akashdeep@Akashs-MacBook-Air.local>
2025-05-13 11:02:00 +0530

    Added post request for Oauth2.0

--- auth.js
+++ auth.js
@@ -1,18 +1,23 @@
 require('dotenv').config()
 const axios= require('axios')
+const jwt = require('jsonwebtoken')
 
 async function getAzureToken(){
     const url = `https://login.microsoftonline.com/${process.env.AZ_TENANT_ID}/oauth2/v2.0/token`;
-    const params = new URLSearchParams()
-    params.append('grant_type', 'client_credentials');
-    params.append('client_id', process.env.AZ_CLIENT_ID);
-    params.append('client_secret', process.env.AZ_CLIENT_SECRET);
-    params.append('scope', process.env.SCOPE);
-    params.append('Content-Type','application/x-www-form-urlencoded')
-    console.log(url,params )
+    const header = {
+        headers : {
+            'Content-Type' : 'application/x-www-form-urlencoded'
+        }
+    }
+    const data = `client_id=${process.env.AZ_CLIENT_ID}&scope=${encodeURIComponent(process.env.SCOPE)}&client_secret=${process.env.AZ_CLIENT_SECRET}&grant_type=client_credentials`;
 
     try {
-        const response = await axios.post(url,params)
+        const response = await axios.post(url, data, {
+            headers : {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        })
+        
         console.log('Access Token:', response.data.access_token);
         return response.data.access_token;
     }catch(err){
